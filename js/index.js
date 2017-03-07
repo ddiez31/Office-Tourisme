ef637a626626dd7edbd19222354979c7405079ac
David <david.diez31@free.fr>
2017-03-07 16:29:20 +0100

    sectorisation

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 (function() {
 
     var app = {
+
+        // carte interactive Carole, Julien
         initmap: function() {
             var map = new L.Map('cdf_map');
             var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
@@ -10,6 +12,21 @@
             map.setView(new L.LatLng(43.1083, 0.7234), 14);
             map.addLayer(osm);
         }
+
+
+
+
+
+
+        // phototheque Odile, Aymeric
+
+
+
+
+        // admin David
+
+
+
 
 
     };
