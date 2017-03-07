940300039a4bc68799c1dcf902df568ffbc18b1b
David <david.diez31@free.fr>
2017-03-07 15:27:18 +0100

    init

---
+++ js/index.js
@@ -0,0 +1,17 @@
+(function() {
+
+    var app = {
+        initmap: function() {
+            var map = new L.Map('cdf_map');
+            var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+            var osmAttrib = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+            var osm = new L.TileLayer(osmUrl, { minZoom: 10, maxZoom: 19, attribution: osmAttrib });
+
+            map.setView(new L.LatLng(43.1083, 0.7234), 14);
+            map.addLayer(osm);
+        }
+
+
+    };
+    app.initmap();
+})();
