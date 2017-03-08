a201a70d0dc4d2ab540d7880f63cb5773fe92403
Julien <julien.linder@gmail.com>
2017-03-08 15:01:51 +0100

    Popup qui marche

--- js/index.js
+++ js/index.js
@@ -11,6 +11,23 @@
 
             map.setView(new L.LatLng(43.1083, 0.7234), 14);
             map.addLayer(osm);
+
+            //var vert
+            //var vert-foncé
+            var ocre = L.circle([43.108053, 0.725208], 5, {
+            color:'#DD985C', fillColor:'f01',fillOpacity: 0.5
+            }).addTo(map);
+
+        //     $(ocre).on("click", function(){
+        //     console.log("OK!") 
+        //     L.responsivePopup().setContent('Coucou.<br>Ça va?').addTo(map).bindPopup();
+        // });
+
+            var circle = L.circle([43.108053, 0.725208]).addTo(map);
+            $(circle).on("click", function(){
+            console.log("OK!") 
+            circle.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+        });
         }
 
 
