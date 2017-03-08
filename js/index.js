cfed78da85e3e6121312c13b231fbefeca71873d
David <david.diez31@free.fr>
2017-03-08 23:13:28 +0100

    get json

--- js/index.js
+++ js/index.js
@@ -225,35 +225,7 @@
                 console.log(marqueur);
             })
 
-
-
-            // $.each(data.Users, function(i) {
-            //     if (data.Users[i].username === inputUsername && data.Users[i].password === inputPassword) {
-            //         // console.log(data.Users[i].color);
-            //         app.fontColor(data.Users[i].color);
-            //     } else {
-            //         return;
-            //     };
-            // });
-        },
-
-        fontColor: function(color) {
-            console.log("fontColor ok");
-            // console.log(color);
-            $(".signin").hide();
-            $(".signout").show();
-            $("*").css("color", color);
-        },
-
-        signout: function() {
-            console.log("signout ok");
-            $("*").removeAttr('style');
-            $(".signout").hide();
-            $(".signin").show();
         }
-
-
-
 
 
     };
