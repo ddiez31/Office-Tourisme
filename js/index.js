81e24293b4be3d5b55a30af3193e3b632340c7ef
Julien <julien.linder@gmail.com>
2017-03-10 12:06:42 +0100

    Julien le popup remarche

--- js/index.js
+++ js/index.js
@@ -22,51 +22,138 @@
                 this.fillColor = '#FF5200';
                 this.fillOpacity = 1;
             };
-            var m1Ocre = new Ocre(43.108053, 0.725208);
-            L.circle([m1Ocre.lat, m1Ocre.long], m1Ocre.size, { color: m1Ocre.color, fillColor: m1Ocre.fillColor, fillOpacity: m1Ocre.fillOpacity }).addTo(map);
 
-            $(Ocre).on("click", function() { //Julien
-                console.log("OK!")
+
+            var m1Ocre = new Ocre(43.108053, 0.725208);
+            m1Ocre = L.circle([m1Ocre.lat, m1Ocre.long], m1Ocre.size, { color: m1Ocre.color, fillColor: m1Ocre.fillColor, fillOpacity: m1Ocre.fillOpacity }).addTo(map);
+
+            $(m1Ocre).on("click", function() { //Julien
                 m1Ocre.bindPopup("<h2>Hello world!</h2>" + "<br>" + '<img src="../images/1.JPG" width="200" height="150">' + '' + '<img src="../images/3.JPG" width="200" height="300">' +
                     "<br><p>I am a popup.</p>").openPopup();
+                console.log("OK!")
+            });
+            //POPUP!!!!!!!
+            // $(Ocre).on("click", function() {
+            //     (this).toggleClass(m1Ocre);
+            // });
+            // $(Ocre).on("click", function(){
+            //     $(this).delegate('m1Ocre', 'on', function(){
+            //         console.log('hello');
+            //     })
+            // })
+            // L.DomEvent.on(Ocre, 'click', function(ev){
+            //     L.DomEvent.stopPropagation(ev);
+            //     console.log("hello")
+            // })
+            //FIN POPUP!!!!!!!!!!
+            var m2Ocre = new Ocre(43.107483, 0.724824);
+            m2Ocre = L.circle([m2Ocre.lat, m2Ocre.long], m2Ocre.size, { color: m2Ocre.color, fillColor: m2Ocre.fillColor, fillOpacity: m2Ocre.fillOpacity }).addTo(map);
+
+            $(m2Ocre).on("click", function() { //Julien
+                m2Ocre.bindPopup("<h2>Hello world!</h2>" + "<br>" + '<img src="../images/1.JPG" width="200" height="150">' + '' + '<img src="../images/3.JPG" width="200" height="300">' +
+                    "<br><p>I am a popup.</p>").openPopup();
+                console.log("OK!")
             });
 
-            var m2Ocre = new Ocre(43.107483, 0.724824);
-            L.circle([m2Ocre.lat, m2Ocre.long], m2Ocre.size, { color: m2Ocre.color, fillColor: m2Ocre.fillColor, fillOpacity: m2Ocre.fillOpacity }).addTo(map);
-
             var m3Ocre = new Ocre(43.107288, 0.724600);
-            L.circle([m3Ocre.lat, m3Ocre.long], m3Ocre.size, { color: m3Ocre.color, fillColor: m3Ocre.fillColor, fillOpacity: m3Ocre.fillOpacity }).addTo(map);
+            m3Ocre = L.circle([m3Ocre.lat, m3Ocre.long], m3Ocre.size, { color: m3Ocre.color, fillColor: m3Ocre.fillColor, fillOpacity: m3Ocre.fillOpacity }).addTo(map);
+
+            $(m3Ocre).on("click", function() { //Julien
+                m3Ocre.bindPopup("<h2>Hello world!</h2>" + "<br>" + '<img src="../images/1.JPG" width="200" height="150">' + '' + '<img src="../images/3.JPG" width="200" height="300">' +
+                    "<br><p>I am a popup.</p>").openPopup();
+                console.log("OK!")
+            });
 
             var m4Ocre = new Ocre(43.107448, 0.725673);
-            L.circle([m4Ocre.lat, m4Ocre.long], m4Ocre.size, { color: m4Ocre.color, fillColor: m4Ocre.fillColor, fillOpacity: m4Ocre.fillOpacity }).addTo(map);
+            m4Ocre = L.circle([m4Ocre.lat, m4Ocre.long], m4Ocre.size, { color: m4Ocre.color, fillColor: m4Ocre.fillColor, fillOpacity: m4Ocre.fillOpacity }).addTo(map);
+
+            $(m4Ocre).on("click", function() { //Julien
+                m4Ocre.bindPopup("<h2>Hello world!</h2>" + "<br>" + '<img src="../images/1.JPG" width="200" height="150">' + '' + '<img src="../images/3.JPG" width="200" height="300">' +
+                    "<br><p>I am a popup.</p>").openPopup();
+                console.log("OK!")
+            });
 
             var m5Ocre = new Ocre(43.107065, 0.725235);
-            L.circle([m5Ocre.lat, m5Ocre.long], m5Ocre.size, { color: m5Ocre.color, fillColor: m5Ocre.fillColor, fillOpacity: m5Ocre.fillOpacity }).addTo(map);
+            m5Ocre = L.circle([m5Ocre.lat, m5Ocre.long], m5Ocre.size, { color: m5Ocre.color, fillColor: m5Ocre.fillColor, fillOpacity: m5Ocre.fillOpacity }).addTo(map);
+
+            $(m5Ocre).on("click", function() { //Julien
+                m5Ocre.bindPopup("<h2>Hello world!</h2>" + "<br>" + '<img src="../images/1.JPG" width="200" height="150">' + '' + '<img src="../images/3.JPG" width="200" height="300">' +
+                    "<br><p>I am a popup.</p>").openPopup();
+                console.log("OK!")
+            });
 
             var m6Ocre = new Ocre(43.107166, 0.725182);
-            L.circle([m6Ocre.lat, m6Ocre.long], m6Ocre.size, { color: m6Ocre.color, fillColor: m6Ocre.fillColor, fillOpacity: m6Ocre.fillOpacity }).addTo(map);
+            m6Ocre = L.circle([m6Ocre.lat, m6Ocre.long], m6Ocre.size, { color: m6Ocre.color, fillColor: m6Ocre.fillColor, fillOpacity: m6Ocre.fillOpacity }).addTo(map);
+
+            $(m6Ocre).on("click", function() { //Julien
+                m6Ocre.bindPopup("<h2>Hello world!</h2>" + "<br>" + '<img src="../images/1.JPG" width="200" height="150">' + '' + '<img src="../images/3.JPG" width="200" height="300">' +
+                    "<br><p>I am a popup.</p>").openPopup();
+                console.log("OK!")
+            });
 
             var m7Ocre = new Ocre(43.107483, 0.725024);
-            L.circle([m7Ocre.lat, m7Ocre.long], m7Ocre.size, { color: m7Ocre.color, fillColor: m7Ocre.fillColor, fillOpacity: m7Ocre.fillOpacity }).addTo(map);
+            m7Ocre = L.circle([m7Ocre.lat, m7Ocre.long], m7Ocre.size, { color: m7Ocre.color, fillColor: m7Ocre.fillColor, fillOpacity: m7Ocre.fillOpacity }).addTo(map);
+
+            $(m7Ocre).on("click", function() { //Julien
+                m7Ocre.bindPopup("<h2>Hello world!</h2>" + "<br>" + '<img src="../images/1.JPG" width="200" height="150">' + '' + '<img src="../images/3.JPG" width="200" height="300">' +
+                    "<br><p>I am a popup.</p>").openPopup();
+                console.log("OK!")
+            });
 
             var m8Ocre = new Ocre(43.106316, 0.720450);
-            L.circle([m8Ocre.lat, m8Ocre.long], m8Ocre.size, { color: m8Ocre.color, fillColor: m8Ocre.fillColor, fillOpacity: m8Ocre.fillOpacity }).addTo(map);
+            m8Ocre = L.circle([m8Ocre.lat, m8Ocre.long], m8Ocre.size, { color: m8Ocre.color, fillColor: m8Ocre.fillColor, fillOpacity: m8Ocre.fillOpacity }).addTo(map);
+
+            $(m8Ocre).on("click", function() { //Julien
+                m8Ocre.bindPopup("<h2>Hello world!</h2>" + "<br>" + '<img src="../images/1.JPG" width="200" height="150">' + '' + '<img src="../images/3.JPG" width="200" height="300">' +
+                    "<br><p>I am a popup.</p>").openPopup();
+                console.log("OK!")
+            });
+
 
             var m9Ocre = new Ocre(43.106210, 0.720790);
-            L.circle([m9Ocre.lat, m9Ocre.long], m9Ocre.size, { color: m9Ocre.color, fillColor: m9Ocre.fillColor, fillOpacity: m9Ocre.fillOpacity }).addTo(map);
+            m9Ocre = L.circle([m9Ocre.lat, m9Ocre.long], m9Ocre.size, { color: m9Ocre.color, fillColor: m9Ocre.fillColor, fillOpacity: m9Ocre.fillOpacity }).addTo(map);
+
+            $(m9Ocre).on("click", function() { //Julien
+                m9Ocre.bindPopup("<h2>Hello world!</h2>" + "<br>" + '<img src="../images/1.JPG" width="200" height="150">' + '' + '<img src="../images/3.JPG" width="200" height="300">' +
+                    "<br><p>I am a popup.</p>").openPopup();
+                console.log("OK!")
+            });
 
             var m10Ocre = new Ocre(43.106436, 0.719825);
-            L.circle([m10Ocre.lat, m10Ocre.long], m10Ocre.size, { color: m10Ocre.color, fillColor: m10Ocre.fillColor, fillOpacity: m10Ocre.fillOpacity }).addTo(map);
+            m10Ocre = L.circle([m10Ocre.lat, m10Ocre.long], m10Ocre.size, { color: m10Ocre.color, fillColor: m10Ocre.fillColor, fillOpacity: m10Ocre.fillOpacity }).addTo(map);
+
+            $(m10Ocre).on("click", function() { //Julien
+                m10Ocre.bindPopup("<h2>Hello world!</h2>" + "<br>" + '<img src="../images/1.JPG" width="200" height="150">' + '' + '<img src="../images/3.JPG" width="200" height="300">' +
+                    "<br><p>I am a popup.</p>").openPopup();
+                console.log("OK!")
+            });
 
             var m11Ocre = new Ocre(43.107202, 0.722536);
-            L.circle([m11Ocre.lat, m11Ocre.long], m11Ocre.size, { color: m11Ocre.color, fillColor: m11Ocre.fillColor, fillOpacity: m11Ocre.fillOpacity }).addTo(map);
+            m11Ocre = L.circle([m11Ocre.lat, m11Ocre.long], m11Ocre.size, { color: m11Ocre.color, fillColor: m11Ocre.fillColor, fillOpacity: m11Ocre.fillOpacity }).addTo(map);
+
+            $(m11Ocre).on("click", function() { //Julien
+                m11Ocre.bindPopup("<h2>Hello world!</h2>" + "<br>" + '<img src="../images/1.JPG" width="200" height="150">' + '' + '<img src="../images/3.JPG" width="200" height="300">' +
+                    "<br><p>I am a popup.</p>").openPopup();
+                console.log("OK!")
+            });
 
             var m12Ocre = new Ocre(43.108430, 0.723737);
-            L.circle([m12Ocre.lat, m12Ocre.long], m12Ocre.size, { color: m12Ocre.color, fillColor: m12Ocre.fillColor, fillOpacity: m12Ocre.fillOpacity }).addTo(map);
+            m12Ocre = L.circle([m12Ocre.lat, m12Ocre.long], m12Ocre.size, { color: m12Ocre.color, fillColor: m12Ocre.fillColor, fillOpacity: m12Ocre.fillOpacity }).addTo(map);
+
+            $(m12Ocre).on("click", function() { //Julien
+                m12Ocre.bindPopup("<h2>Hello world!</h2>" + "<br>" + '<img src="../images/1.JPG" width="200" height="150">' + '' + '<img src="../images/3.JPG" width="200" height="300">' +
+                    "<br><p>I am a popup.</p>").openPopup();
+                console.log("OK!")
+            });
 
             var m13Ocre = new Ocre(43.109000, 0.724839);
-            L.circle([m13Ocre.lat, m13Ocre.long], m13Ocre.size, { color: m13Ocre.color, fillColor: m13Ocre.fillColor, fillOpacity: m13Ocre.fillOpacity }).addTo(map);
+            m13Ocre = L.circle([m13Ocre.lat, m13Ocre.long], m13Ocre.size, { color: m13Ocre.color, fillColor: m13Ocre.fillColor, fillOpacity: m13Ocre.fillOpacity }).addTo(map);
 
+            $(m13Ocre).on("click", function() { //Julien
+                m13Ocre.bindPopup("<h2>Hello world!</h2>" + "<br>" + '<img src="../images/1.JPG" width="200" height="150">' + '' + '<img src="../images/3.JPG" width="200" height="300">' +
+                    "<br><p>I am a popup.</p>").openPopup();
+                console.log("OK!")
+            });
            
 
 
