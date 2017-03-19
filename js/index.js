dbe1a8c4c364ad96b6eed4ccaaee2989ee61050b
David <david.diez31@free.fr>
2017-03-19 21:51:14 +0100

    refactoring

--- js/index.js
+++ js/index.js
@@ -70,30 +70,33 @@
                 }]
             }).addTo(map);
 
+
+            //définition des marqueurs
+            var Marker = function(text, borderColor, backgroundColor, textColor) {
+                this.text = text;
+                this.iconSize = [15, 15];
+                this.borderColor = borderColor;
+                this.backgroundColor = backgroundColor;
+                this.textColor = textColor;
+                this.isAlphaNumericIcon = true;
+                this.innerIconStyle = 'margin:auto';
+            };
+
             //circuit ocre
             for (i = 0; i < data.ocre.length; i++) {
                 //marqueurs
-                var Ocre = function() {
-                    this.text = data.ocre[i].marqueur;
-                    this.iconSize = [15, 15];
-                    this.borderColor = '#FF5200';
-                    this.backgroundColor = "rgba(255, 82, 0, 0.5)";
-                    this.textColor = '#000';
-                    this.isAlphaNumericIcon = true;
-                    this.innerIconStyle = 'margin:auto';
-                };
-                var markOcre = new Ocre();
+                var markersOcre = new Marker(data.ocre[i].marqueur, '#FF5200', "rgba(255, 82, 0, 0.5)", '#000');
                 var latOcre = data.ocre[i].geoloc.lat;
                 var longOcre = data.ocre[i].geoloc.lng;
-                markOcre = L.marker([latOcre, longOcre], {
+                markersOcre = L.marker([latOcre, longOcre], {
                     icon: L.BeautifyIcon.icon({
-                        iconSize: markOcre.iconSize,
-                        borderColor: markOcre.borderColor,
-                        backgroundColor: markOcre.backgroundColor,
-                        isAlphaNumericIcon: markOcre.isAlphaNumericIcon,
-                        text: markOcre.text,
-                        textColor: markOcre.textColor,
-                        innerIconStyle: markOcre.innerIconStyle
+                        iconSize: markersOcre.iconSize,
+                        borderColor: markersOcre.borderColor,
+                        backgroundColor: markersOcre.backgroundColor,
+                        isAlphaNumericIcon: markersOcre.isAlphaNumericIcon,
+                        text: markersOcre.text,
+                        textColor: markersOcre.textColor,
+                        innerIconStyle: markersOcre.innerIconStyle
                     })
                 }).addTo(map);
                 //popup
@@ -106,7 +109,7 @@
                     contentPopupOcre += '<a href="' + imgOcre + '" data-lightbox="' + imgOcre + '"><img class="imgPopup" src="' + imgOcre + '" alt="' + titreOcre + '" data-image="' + imgOcre + '"></a>';
                 }
                 contentPopupOcre += "</div><p>" + texteOcre + "</p>";
-                markOcre.bindPopup(contentPopupOcre);
+                markersOcre.bindPopup(contentPopupOcre);
             };
             //sentier
             var latlngsOcre = data.ocre[0].sentier;
@@ -115,28 +118,19 @@
 
             //circuit vert clair
             for (i = 0; i < data.vert.length; i++) {
-                //marqueurs
-                var Vert = function() {
-                    this.text = data.vert[i].marqueur;
-                    this.iconSize = [15, 15];
-                    this.borderColor = '#18453B';
-                    this.backgroundColor = "rgb(0, 171, 57, 0.5)";
-                    this.textColor = '#000';
-                    this.isAlphaNumericIcon = true;
-                    this.innerIconStyle = 'margin:auto';
-                };
-                var markVert = new Vert();
+                //marqueurs                 
+                var markersVert = new Marker(data.vert[i].marqueur, '#18453B', "rgba(0, 171, 57, 0.5)", '#000');
                 var latVert = data.vert[i].geoloc.lat;
                 var longVert = data.vert[i].geoloc.lng;
-                markVert = L.marker([latVert, longVert], {
+                markersVert = L.marker([latVert, longVert], {
                     icon: L.BeautifyIcon.icon({
-                        iconSize: markVert.iconSize,
-                        borderColor: markVert.borderColor,
-                        backgroundColor: markVert.backgroundColor,
-                        isAlphaNumericIcon: markVert.isAlphaNumericIcon,
-                        text: markVert.text,
-                        textColor: markVert.textColor,
-                        innerIconStyle: markVert.innerIconStyle
+                        iconSize: markersVert.iconSize,
+                        borderColor: markersVert.borderColor,
+                        backgroundColor: markersVert.backgroundColor,
+                        isAlphaNumericIcon: markersVert.isAlphaNumericIcon,
+                        text: markersVert.text,
+                        textColor: markersVert.textColor,
+                        innerIconStyle: markersVert.innerIconStyle
                     })
                 }).addTo(map);
                 //popup
@@ -149,7 +143,7 @@
                     contentPopupVert += '<a href="' + imgVert + '" data-lightbox="' + imgVert + '"><img class="imgPopup" src="' + imgVert + '" alt="' + titreVert + '" data-image="' + imgVert + '"></a>';
                 }
                 contentPopupVert += "<p>" + texteVert + "</p>";
-                markVert.bindPopup(contentPopupVert);
+                markersVert.bindPopup(contentPopupVert);
             };
             //sentier
             var latlngsVert = data.vert[0].sentier;
@@ -159,27 +153,18 @@
             //batiments officiels
             for (i = 0; i < data.annexes.length; i++) {
                 //marqueurs
-                var Annexes = function() {
-                    this.text = data.annexes[i].marqueur;
-                    this.iconSize = [15, 15];
-                    this.borderColor = ' #193025';
-                    this.backgroundColor = "rgb(0, 100, 0, 0.5)";
-                    this.textColor = '#000';
-                    this.isAlphaNumericIcon = true;
-                    this.innerIconStyle = 'margin:auto';
-                };
-                var markAnnexes = new Annexes();
+                var markersAnnexes = new Marker(data.annexes[i].marqueur, '#193025', "rgba(0, 100, 0, 0.5)", '#000');
                 var latAnnexes = data.annexes[i].geoloc.lat;
                 var longAnnexes = data.annexes[i].geoloc.lng;
-                markAnnexes = L.marker([latAnnexes, longAnnexes], {
+                markersAnnexes = L.marker([latAnnexes, longAnnexes], {
                     icon: L.BeautifyIcon.icon({
-                        iconSize: markAnnexes.iconSize,
-                        borderColor: markAnnexes.borderColor,
-                        backgroundColor: markAnnexes.backgroundColor,
-                        isAlphaNumericIcon: markAnnexes.isAlphaNumericIcon,
-                        text: markAnnexes.text,
-                        textColor: markAnnexes.textColor,
-                        innerIconStyle: markAnnexes.innerIconStyle
+                        iconSize: markersAnnexes.iconSize,
+                        borderColor: markersAnnexes.borderColor,
+                        backgroundColor: markersAnnexes.backgroundColor,
+                        isAlphaNumericIcon: markersAnnexes.isAlphaNumericIcon,
+                        text: markersAnnexes.text,
+                        textColor: markersAnnexes.textColor,
+                        innerIconStyle: markersAnnexes.innerIconStyle
                     })
                 }).addTo(map);
                 //popup
@@ -192,25 +177,20 @@
                     contentPopupAnnexes += '<a href="' + imgAnnexes + '" data-lightbox="' + imgAnnexes + '"><img class="imgPopup" src="' + imgAnnexes + '" alt="' + titreAnnexes + '" data-image="' + imgAnnexes + '"></a>';
                 }
                 contentPopupAnnexes += "</div><p>" + texteAnnexes + "</p>";
-                markAnnexes.bindPopup(contentPopupAnnexes);
+                markersAnnexes.bindPopup(contentPopupAnnexes);
             };
 
             //batiments etoiles
             for (i = 0; i < data.etoiles.length; i++) {
                 //marqueurs
-                var Etoiles = function() {
-                    this.borderColor = data.etoiles[i].marqueur;
-                    this.backgroundColor = data.etoiles[i].marqueur;
-                    this.iconSize = [8, 8];
-                };
-                var markEtoiles = new Etoiles();
+                var markersEtoiles = new Marker('', data.etoiles[i].marqueur, data.etoiles[i].marqueur);
                 var latEtoiles = data.etoiles[i].geoloc.lat;
                 var longEtoiles = data.etoiles[i].geoloc.lng;
-                markEtoiles = L.marker([latEtoiles, longEtoiles], {
+                markersEtoiles = L.marker([latEtoiles, longEtoiles], {
                     icon: L.BeautifyIcon.icon({
-                        iconSize: markEtoiles.iconSize,
-                        borderColor: markEtoiles.borderColor,
-                        backgroundColor: markEtoiles.backgroundColor
+                        iconSize: [8, 8],
+                        borderColor: markersEtoiles.borderColor,
+                        backgroundColor: markersEtoiles.backgroundColor
                     })
                 }).addTo(map);
                 //popup
@@ -223,7 +203,7 @@
                     contentPopupEtoiles += '<a href="' + imgEtoiles + '" data-lightbox="' + imgEtoiles + '"><img class="imgPopup" src="' + imgEtoiles + '" alt="' + titreEtoiles + '" data-image="' + imgEtoiles + '"></a>';
                 }
                 contentPopupEtoiles += "</div><p>" + texteEtoiles + "</p>";
-                markEtoiles.bindPopup(contentPopupEtoiles);
+                markersEtoiles.bindPopup(contentPopupEtoiles);
             };
 
             //filtres sentiers
@@ -276,41 +256,7 @@
 
             $('#gallery').html(photoOcre + photoVert + photoAnnexes + photoEtoiles);
 
-            $("#gallery").unitegallery({
-                //main options:
-                grid_padding: 10, //set padding to the grid
-                grid_space_between_cols: 10, //space between columns
-                grid_space_between_rows: 10, //space between rows
-                //gallery options:
-                gallery_theme: "tilesgrid", //choose gallery theme (if more then one themes includes)
-                gallery_width: "100%", //gallery width
-                gallery_background_color: "œ#C0C0C0", //set custom background color. If not set it will be taken from css.
-                //navigation option:
-                grid_num_rows: 3, //maximum number of grid rows. If set to big value, the navigation will not appear.
-                theme_navigation_type: "arrows", //bullets, arrows
-                theme_space_between_arrows: 5, //horizontal space between arrows
-                //tile design options:
-                //tiles_type: "justified", //must option for the tiles - justified type
-                // tiles_justified_row_height: 120, //base row height of the justified type
-                // tiles_justified_space_between: 3, //space between the tiles justified type
-                // tiles_set_initial_height: true, //columns type related only
-                // tiles_enable_transition: true, //enable transition when screen width change
-                tile_overlay_opacity: 0.4, //tile overlay opacity
-                tile_overlay_color: "#000000", //tile overlay color
-                tile_enable_image_effect: false, //enable tile image effect
-                //tile text panel options:
-                tile_enable_textpanel: true,
-                tile_textpanel_title_text_align: "center",
-                tile_textpanel_always_on: true,
-                tile_textpanel_title_font_size: null, //textpanel title font size. if null - take from css
-                tile_enable_action: true,
-                //lightbox options:
-                lightbox_slider_control_zoom: false, //true, false - enable zooming control
-                lightbox_textpanel_enable_description: true, //enable the description text
-                lightbox_type: "compact", //compact / wide - lightbox type
-                lightbox_overlay_opacity: 0.8, //the opacity of the overlay. for compact type - 0.6
-                lightbox_slider_image_border: false, //enable border around the image (for compact type only)
-            });
+            unitegallery();
 
             select(photoOcre, photoVert, photoAnnexes, photoEtoiles);
             $(".ui-dialog-titlebar-close").on("click", function() {
@@ -321,6 +267,7 @@
     app.init();
 })();
 
+//filtre phototheque
 function select(photoOcre, photoVert, photoAnnexes, photoEtoiles) {
     $("#selectTheme").on("change", function() {
         if ($(this).val() == "Ocre") {
@@ -334,41 +281,40 @@ function select(photoOcre, photoVert, photoAnnexes, photoEtoiles) {
         } else {
             $('#gallery').html(photoOcre + photoVert + photoAnnexes + photoEtoiles);
         };
+        unitegallery();
+    });
+};
 
-        $("#gallery").unitegallery({
-            //main options:
-            grid_padding: 10, //set padding to the grid
-            grid_space_between_cols: 10, //space between columns
-            grid_space_between_rows: 10, //space between rows
-            //gallery options:
-            gallery_theme: "tilesgrid", //choose gallery theme (if more then one themes includes)
-            gallery_width: "100%", //gallery width
-            gallery_background_color: "œ#C0C0C0", //set custom background color. If not set it will be taken from css.
-            //navigation option:
-            grid_num_rows: 3, //maximum number of grid rows. If set to big value, the navigation will not appear.
-            theme_navigation_type: "arrows", //bullets, arrows
-            theme_space_between_arrows: 5, //horizontal space between arrows
-            //tile design options:
-            //tiles_type: "justified", //must option for the tiles - justified type
-            // tiles_justified_row_height: 120, //base row height of the justified type
-            // tiles_justified_space_between: 3, //space between the tiles justified type
-            // tiles_set_initial_height: true, //columns type related only
-            // tiles_enable_transition: true, //enable transition when screen width change
-            tile_overlay_opacity: 0.4, //tile overlay opacity
-            tile_overlay_color: "#000000", //tile overlay color
-            tile_enable_image_effect: false, //enable tile image effect
-            //tile text panel options:
-            tile_enable_textpanel: true,
-            tile_textpanel_title_text_align: "center",
-            tile_textpanel_always_on: true,
-            tile_textpanel_title_font_size: null, //textpanel title font size. if null - take from css
-            tile_enable_action: true,
-            //lightbox options:
-            lightbox_slider_control_zoom: false, //true, false - enable zooming control
-            lightbox_textpanel_enable_description: true, //enable the description text
-            lightbox_type: "compact", //compact / wide - lightbox type
-            lightbox_overlay_opacity: 0.8, //the opacity of the overlay. for compact type - 0.6
-            lightbox_slider_image_border: false, //enable border around the image (for compact type only)
-        });
-    })
+//définition galerie
+function unitegallery() {
+    $("#gallery").unitegallery({
+        //main options:
+        grid_padding: 10,
+        grid_space_between_cols: 10,
+        grid_space_between_rows: 10,
+        //gallery options:
+        gallery_theme: "tilesgrid",
+        gallery_width: "100%",
+        gallery_background_color: "œ#C0C0C0",
+        //navigation option:
+        grid_num_rows: 3,
+        theme_navigation_type: "arrows",
+        theme_space_between_arrows: 5,
+        //tile design options:
+        tile_overlay_opacity: 0.4,
+        tile_overlay_color: "#000000",
+        tile_enable_image_effect: false,
+        //tile text panel options:
+        tile_enable_textpanel: true,
+        tile_textpanel_title_text_align: "center",
+        tile_textpanel_always_on: true,
+        tile_textpanel_title_font_size: null,
+        tile_enable_action: true,
+        //lightbox options:
+        lightbox_slider_control_zoom: false,
+        lightbox_textpanel_enable_description: true,
+        lightbox_type: "compact",
+        lightbox_overlay_opacity: 0.8,
+        lightbox_slider_image_border: false
+    });
 };
